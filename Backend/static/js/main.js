995940839cbcbe005744010abf989009c12cb3fc
benar <mulwabenard24@gmail.com>
2024-07-18 23:45:29 +0300

    integrate the deposit and send endpoints to the front

--- Backend/static/js/main.js
+++ Backend/static/js/main.js
@@ -15,5 +15,49 @@ closeBtn.addEventListener("click",()=>{
 
 function mySubmitFunction(e) {
     e.preventDefault();
+    const depositData = {
+        'receiver_account_number': document.getElementById('receiver-account-number').value,
+        'amount': document.getElementById('sending-amount').value,
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/send/',
+        data: JSON.stringify(depositData),
+        contentType: 'application/json',
+        success: function (response) {
+            console.log(response)
+            if (response.status == 200) {
+                alert(response.message)
+            }
+        },
+        error: function (xhr, errmsg, err) {
+            console.log(xhr.status + ": " + xhr.responseText);
+        }
+    });
+
+function myDepositFunction(e) {
+        e.preventDefault();
+        const depositData = {
+            'amount': document.getElementById('self-deposit-amount').value,
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/deposit/',
+            data: JSON.stringify(depositData),
+            contentType: 'application/json',
+            success: function (response) {
+                console.log(response)
+                if (response.status == 200) {
+                    alert(response.message)
+                }
+            },
+            error: function (xhr, errmsg, err) {
+                console.log(xhr.status + ": " + xhr.responseText);
+            }
+        });
+    }
+
     return false;
   }
